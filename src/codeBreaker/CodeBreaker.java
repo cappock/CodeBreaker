e532d0d36298f951a384388aa855f505fa88aec2
cappock <hector.guerra@udea.edu.co>
2019-08-19 15:07:45 -0500

    update

--- src/codeBreaker/CodeBreaker.java
+++ src/codeBreaker/CodeBreaker.java
@@ -4,56 +4,81 @@ import java.util.ArrayList;
 
 
 public class CodeBreaker {
-
+	
+	
+	private ArrayList<Integer> numToGuess;
+	
 	public static void main(String[] args) {
+		
 	}
+	
+	public CodeBreaker(int num) {
+		numToGuess = new ArrayList<Integer>();
+		numToGuess.add(num/1000 % 10);
+		numToGuess.add(num/100 % 10);
+		numToGuess.add(num/10 % 10);
+		numToGuess.add(num % 10);
+	}
+	
+	public CodeBreaker() {
+		numToGuess = new ArrayList<Integer>();
+		numToGuess.add(1);
+		numToGuess.add(2);
+		numToGuess.add(3);
+		numToGuess.add(4);
+	}
+	
+	
 	
 	public String confirmar(int[] r) {
 		String respuesta = "";
-		
+		String countEmpty = "";
 		if(r.length != 4) {
 			return "El array debe ser de 4 digitos";
 		}
  
-		ArrayList<Integer> num = new ArrayList<Integer>(); 
-		num.add(1);
-		num.add(2);
-		num.add(3);
-		num.add(4);
-
 		for(int i = 0 ; i < r.length; i++ ) {
 			switch (i) {
 			case 0:
-				if(r[i]==num.get(0)) {
-					respuesta=respuesta+" X";
-				}else if(num.contains(r[i])){
-					respuesta =respuesta + " _";
-
+				if(r[i]==numToGuess.get(0)) {
+					respuesta="X"+respuesta;
+				}else if(numToGuess.contains(r[i])){
+					respuesta =respuesta + "_";
+				}else {
+					countEmpty += " ";
 				}
 				break;
 			case 1:
-				if(r[i]==num.get(1)) {
-					respuesta=respuesta+" X";
-				}else if(num.contains(r[i])){
-					respuesta =respuesta + " _";
+				if(r[i]==numToGuess.get(1)) {
+					respuesta= "X"+respuesta;
+				}else if(numToGuess.contains(r[i])){
+					respuesta =respuesta + "_";
+				}else {
+					countEmpty += " ";
 				}
 				break;
 			case 2:
-				if(r[i]==num.get(2)) {
-					respuesta=respuesta+" X";
-				}else if(num.contains(r[i])){
-					respuesta =respuesta + " _";
+				if(r[i]==numToGuess.get(2)) {
+					respuesta="X"+respuesta;
+				}else if(numToGuess.contains(r[i])){
+					respuesta =respuesta + "_";
+				}else {
+					countEmpty += " ";
 				}
 				break;
 			case 3:
-				if(r[i]==num.get(3)) {
-					respuesta=respuesta+" X";
-				}else if(num.contains(r[i])){
-					respuesta =respuesta + " _";
+				if(r[i]==numToGuess.get(3)) {
+					respuesta="X"+respuesta;
+				}else if(numToGuess.contains(r[i])){
+					respuesta =respuesta + "_";
+				}else {
+					countEmpty += " ";
 				}
 				break;
 			}			
 		}
+		
+		respuesta = respuesta + countEmpty;
 		return respuesta;
 	}
 
--- src/codeBreakerTest/CodeBreakerTest.java
+++ src/codeBreakerTest/CodeBreakerTest.java
@@ -2,6 +2,8 @@ package codeBreakerTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 import codeBreaker.CodeBreaker;
@@ -13,21 +15,21 @@ class CodeBreakerTest {
 		cb = new CodeBreaker();
 		int num[] = new int[] {1,2,3,4};
 		String r = cb.confirmar(num);
-		assertEquals(" X X X X", r);
+		assertEquals("XXXX", r);
 	}
 	@Test
 	void testNoMatch() {
 		cb = new CodeBreaker();
 		int num[] = new int[] {5,6,7,8};
 		String r = cb.confirmar(num);
-		assertEquals("", r);
+		assertEquals("    ", r);
 	}
 	@Test
 	void testAllNumberMatch() {
 		cb = new CodeBreaker();
 		int num[] = new int[] {3,1,4,2};
 		String r = cb.confirmar(num);
-		assertEquals(" _ _ _ _", r);
+		assertEquals("____", r);
 	}
 	
 	@Test
@@ -35,7 +37,7 @@ class CodeBreakerTest {
 		cb = new CodeBreaker();
 		int num[] = new int[] {2, 1, 4, 5};
 		String r = cb.confirmar(num);
-		assertEquals(" _ _ _", r);
+		assertEquals("___ ", r);
 	}
 	
 	@Test
@@ -59,7 +61,7 @@ class CodeBreakerTest {
 		cb = new CodeBreaker();
 		int num[] = new int[] {1, 2, 4, 3};
 		String r = cb.confirmar(num);
-		assertEquals(" X X _ _", r);
+		assertEquals("XX__", r);
 	}
 	
 	@Test
@@ -67,7 +69,59 @@ class CodeBreakerTest {
 		cb = new CodeBreaker();
 		int num[] = new int[] {1, 2, 4, 3};
 		String r = cb.confirmar(num);
-		assertEquals(" X X _ _", r);
+		assertEquals("XX__", r);
 	}
 	
+	@Test
+	void test_GetOtherNumber() {
+		cb = new CodeBreaker(2467);
+		int num[] = new int[] {2, 4, 6, 7};
+		String r = cb.confirmar(num);
+		assertEquals("XXXX", r);
+	}
+	
+	@Test
+	void test_Priority() {
+		cb = new CodeBreaker();
+		int num[] = new int[] {1, 3, 2, 4};
+		String r = cb.confirmar(num);
+		assertEquals("XX__", r);
+	}
+	
+	@Test
+	void test_GetOtherNumberNoMatch() {
+		cb = new CodeBreaker(2467);
+		int num[] = new int[] {1, 5, 8, 9};
+		String r = cb.confirmar(num);
+		assertEquals("    ", r);
+	}
+	@Test
+	void test_GetOtherNumberDiffPosition() {
+		cb = new CodeBreaker(2467);
+		int num[] = new int[] {6, 2, 7, 4};
+		String r = cb.confirmar(num);
+		assertEquals("____", r);
+	}
+	
+	@Test
+	void test_GetOtherNumberSomeMatch() {
+		cb = new CodeBreaker(2467);
+		int num[] = new int[] {2, 8, 9, 7};
+		String r = cb.confirmar(num);
+		assertEquals("XX  ", r);
+	}
+	@Test
+	void test_GetOtherNumberSomePosition() {
+		cb = new CodeBreaker(2467);
+		int num[] = new int[] {8, 2, 7, 9};
+		String r = cb.confirmar(num);
+		assertEquals("__  ", r);
+	}
+	@Test
+	void test_GetOtherNumberSomePositionAndMatch() {
+		cb = new CodeBreaker(2467);
+		int num[] = new int[] {2, 4, 7, 6};
+		String r = cb.confirmar(num);
+		assertEquals("XX__", r);
+	}
 }
